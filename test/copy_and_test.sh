c6de98be047c5e9520870505e47b25250f966862
Eric Cousineau <eric.cousineau@tri.global>
2018-01-07 01:09:42 -0500

    Simplify

--- test/copy_and_test.sh
+++ test/copy_and_test.sh
@@ -7,22 +7,18 @@ if [[ ! $(basename $(dirname ${PWD})) =~ .*\.runfiles ]]; then
     exit 1
 fi
 
-cmd=$(eval echo ${1})
-echo ${cmd}
+cmd=${1}
 pkg_reldir=${2}
 shift && shift
 extra_dirs="$@"
 
-export COPY_AND_TEST=1
 export TMP_BASE=/tmp/bazel_external_data
 mkdir -p ${TMP_BASE}
 export TMP_DIR=$(mktemp -d -p ${TMP_BASE})
 
+# Copy what's needed for a modifiable `bazel_pkg_advanced_test` directory.
 mock_dir=${TMP_DIR}/mock_workspace
 
-rm -rf ${mock_dir}
-
-# Copy what's needed for a modifiable `bazel_pkg_advanced_test` directory.
 srcs="src tools BUILD.bazel WORKSPACE ${pkg_reldir} ${extra_dirs}"
 mkdir -p ${mock_dir}
 readlink_py() { python -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' ${1}; }
@@ -32,5 +28,6 @@ for src in ${srcs}; do
     cp -r $(readlink_py ${src}) ${mock_dir}/${subdir}
 done
 
+# Change to the workspace directory, and begin.
 cd ${mock_dir}/${pkg_reldir}
-eval ${cmd}
+COPY_AND_TEST=1 eval ${cmd}
