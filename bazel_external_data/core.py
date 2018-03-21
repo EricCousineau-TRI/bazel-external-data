2a376022927452c671ced4a755898ee989596663
Eric Cousineau <eric.cousineau@tri.global>
2018-03-21 14:58:23 -0400

    fxiit

--- bazel_external_data/core.py
+++ bazel_external_data/core.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import stat
+import subprocess
 
 from bazel_external_data import util, config_helpers, hashes
 
@@ -124,6 +125,17 @@ class Project(object):
         """Writes hashsum for a given set of file information. """
         self._frontend.update_hash_file_info(info.orig_filepath, hash)
 
+    def get_registered_files(self, use_relpath=False):
+        """Returns a list of relpaths of files contained within the project."""
+        output = subprocess.check_output(
+            "find '{root}' -name '*.sha512'".format(root=self.root_path),
+            shell=True)
+        files = self._frontend.find_registered_file_abspaths(self.root_path)
+        if use_relpath:
+            return [self._get_relpath(file) for file in files]
+        else:
+            return files
+
 
 class User(object):
     """Stores user-level configuration. """
@@ -153,6 +165,14 @@ class HashFileFrontend(object):
     def _get_hash_file(self, input_file):
         assert not self._is_hash_file(input_file)
         return input_file + self._suffix
+
+    def find_registered_file_abspaths(self, start_dir):
+        """Gets all registered file abspaths."""
+        output = subprocess.check_output(
+            "find '{root}' -name '*{suffix}'".format(
+                root=start_dir, suffix=self._suffix),
+            shell=True)
+        return output.strip().split("\n")
 
     def get_hash_file_info(self, input_file, needs_hash):
         """Gets hash file information. """
--- bazel_external_data/squash.py
+++ bazel_external_data/squash.py
@@ -9,11 +9,12 @@ files for `base`. These files are staged into `merge`.
 # be generalized to be Girder-agnostic.
 
 import os
-import subprocess
 import sys
+from tempfile import mkdtemp
 import yaml
 
 from bazel_external_data.core import load_project
+from bazel_external_data.util import eprint
 
 
 def add_arguments(parser):
@@ -27,44 +28,46 @@ def add_arguments(parser):
              "which are new to `base`")
     parser.add_argument(
         "--files", type=str, nargs='*', default=None,
-        help="Files to check. By default, checks all files.")
-    parser.add_argument(
-        "--stage_dir", type=str, default="/tmp/bazel_external_data/merge",
-        help="Staging directory for temporarily downloading files")
+        help="Files to check. By default, checks all files in the project.")
+
 
 def run(args, project):
     # Ensure that all remotes are disjoint.
     assert args.base != args.head and args.head != args.merge, (
         "Must supply unique remotes")
 
+    if args.verbose:
+        print("base: {}".format(args.base))
+        print("head: {}".format(args.head))
+        print("merge: {}".format(args.merge))
+
     # Remotes.
     base = project._get_remote(args.base)
     head = project._get_remote(args.head)
     merge = project._get_remote(args.merge)
 
+    stage_dir = mkdtemp(prefix="bazel_external_data-merge-")
+
+    if args.verbose:
+        print("stage_dir: {}".format(stage_dir))
+
     # List files.
-    # TOOD(eric.cousineau): Move this to the project.
     if args.files is None:
-        output = subprocess.check_output(
-            "find '{root}' -name '*.sha512'".format(root=project.root_path),
-            shell=True)
-        files = output.strip().split("\n")
+        files = project.get_registered_files()
     else:
         files = [os.path.abspath(file) for file in args.files]
 
-    for file_abspath in files:
-        print(file_abspath)
-        info = project.get_file_info(file_abspath, needs_hash=True)
+    def do_squash(info):
         if args.verbose:
             yaml.dump(
                 info.debug_config(), sys.stdout, default_flow_style=False)
         # If the file already exists in `base`, no need to do anything.
         if base.check_file(info.hash, info.project_relpath):
             print("- Skip: {}".format(info.project_relpath))
-            continue
+            return
         # File not already uploaded: download from `head` to `stage_dir`, then
         # upload to `merge`.
-        file_stage_abspath = os.path.join(args.stage_dir, info.project_relpath)
+        file_stage_abspath = os.path.join(stage_dir, info.project_relpath)
         file_stage_dir = os.path.dirname(file_stage_abspath)
         if not os.path.exists(file_stage_dir):
             os.makedirs(file_stage_dir)
@@ -75,3 +78,19 @@ def run(args, project):
             info.hash.hash_type, info.project_relpath, file_stage_abspath)
         assert info.hash == hash_merge  # Sanity check
         print("Uploaded: {}".format(info.project_relpath))
+
+    good = True
+    for file_abspath in files:
+        info = project.get_file_info(file_abspath, needs_hash=True)
+        def action():
+            do_squash(info)
+        if args.keep_going:
+            try:
+                action()
+            except RuntimeError as e:
+                good = False
+                eprint(e)
+                eprint("Continuing (--keep_going)")
+        else:
+            action()
+    return good
